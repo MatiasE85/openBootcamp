cdcb385a3294ab26cf11878e48fea672dbc462e5
Matias <matiase85@gmail.com>
2022-06-19 14:24:16 -0300

    Ejercicios resuelto

---
+++ ejercicioTema3.js
@@ -0,0 +1,23 @@
+//Primera parte
+var sumarDosNumero = function(a, b, c) {
+    suma = a + b + c;
+    return suma;
+}
+sumarDosNumero(4, 7, 25);
+
+//Segunda parte
+
+class Coche {
+    miCoche(puertas) {
+        this.puertas = puertas;
+        this.agregarPuertas = function() {
+            return puertas++
+        }
+    }
+}
+
+//Quiero destacar que prefiero el uso de Javascript y no de Java, ya que mi hoja de
+//ruta corresponde react y nodeJs. Los conceptos los tengo claros y, aunque la parte
+//dos del ejercicio no esté del todo correcta, las ideas si están claras. 
+//Recomiendo que estos ejercicios también se hagan en el lenguaje de Javascript, sería
+//ideal para gente que ya está un poco en el tema y puede avanzar un poco mas rapido.

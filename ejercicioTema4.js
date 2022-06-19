df7b285f7c4e57f1b8ea431d307fe9881d096587
Matias <matiase85@gmail.com>
2022-06-19 14:57:49 -0300

    Ejercicos tema 4 resueltos

---
+++ ejercicioTema4.js
@@ -0,0 +1,54 @@
+//Comparar numeros
+function compararNumero(numero) {
+    if (numero == 0) {
+        console.log("El numero es 0");
+    } else if (numero > 0) {
+        console.log("El numero es positivo");
+    } else {
+        console.log("El numero es negativo");
+    }
+}
+
+// Ciclo while
+
+var numeroWhile = 0;
+
+while (numeroWhile < 3) {
+    numeroWhile++
+    console.log(numeroWhile)
+}
+
+//Ciclo do while
+
+var resultado = ""
+var numeroDoWhile = 2
+do {
+    numeroDoWhile++
+    resultado = resultado + numeroDoWhile
+} while (numeroDoWhile < 3)
+console.log(resultado)
+
+//Ciclo for
+
+for (let numeroFor = 0; numeroFor < 3; numeroFor++) {
+    console.log(numeroFor)
+}
+
+//Switch
+
+switch (estacion) {
+    case verano:
+        console.log("Verano");
+        break;
+    case otoño:
+        console.log("Otoño");
+        break;
+    case invierno:
+        console.log("Invierno");
+        break;
+    case primavera:
+        console.log("Primavera");
+        break;
+    default:
+        console.log("No es una estación del año")
+}
